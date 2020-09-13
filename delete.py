effc7605a61e0b2939174f8f67ed7bd2c8fa3864
DanielSilva23 <dannysilva12345@gmail.com>
2020-09-13 14:42:43 -0400

    that is it for now, all good the api key has been attached ... delete all is cluncky

--- delete.py
+++ delete.py
@@ -1,5 +1,5 @@
 from decimalencoder import DecimalEncoder
-
+from handler import response_dump
 import boto3
 dynamodb = boto3.resource('dynamodb')
 
@@ -20,3 +20,18 @@ def delete_single(event, context):
     }
 
     return response
+
+
+def delete_content(event, context):
+    table = dynamodb.Table('posts')
+    result = table.scan()
+
+    for instance in result['Items']:
+        table.delete_item(
+            Key={
+                'id': instance['id']
+            }
+        )
+
+    return response_dump(200, {"message": "All instances in the posts table have been deleted"})
+
